a46a6ca231ca13c8d3c3962937cea9de5d54fae7
hhhamada <hmdhrk.71@gmail.com>
2022-09-15 17:15:55 +0900

    add user

--- code/user.py
+++ code/user.py
@@ -3,3 +3,14 @@ class User(object):
         self.id = _id
         self.username = username
         self.password = password
+
+class Reserver(object):
+    def __init__(self, reserver_id, reserver_name, reserver_mailaddress):
+        self.id = reserver_id
+        self.name = reserver_name
+        self.mail_address = reserver_mailaddress
+
+class Nurse(object):
+    def __init__(self, nurse_id, name):
+        self.id = nurse_id
+        self.name = name
